1cf58f6b758af113efa8b6960e733c6c08acf0a6
Daniel <dkweonlee@gmail.com>
2017-04-03 12:01:50 -0500

    add friendly_id to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403155840) do
+ActiveRecord::Schema.define(version: 20170403165733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20170403155840) do
     t.string   "link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
+    t.string   "string"
+    t.index ["string"], name: "index_projects_on_string", unique: true, using: :btree
   end
 
 end
